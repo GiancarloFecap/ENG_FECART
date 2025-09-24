3ad907d772ee0e824c4eaf31fc81cf88a58395a9
GiancarloFecap <giancarlofecap@gmail.com>
2025-09-24 14:28:50 -0300

    changes

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/teste')
-def test():
-    return render_template('teste.html')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
